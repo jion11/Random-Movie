752b4157c3bbf68cc64dca2e1a728dfe499376a0
Cameron <cameron.ohree@gmail.com>
2022-08-10 04:30:39 -0400

    remove movies from watchlist

--- src/main/java/com/Movie/Randomizer/MovieRandomizerController.java
+++ src/main/java/com/Movie/Randomizer/MovieRandomizerController.java
@@ -70,7 +70,7 @@ public class MovieRandomizerController {
 
     }
 
-    public void deleteMovie(@RequestParam String movieToDelete) {
+    public void deleteMovie(@RequestParam("movieToDelete") String movieToDelete) {
         System.out.println(movieToDelete);
         movieRandomizerService.deleteMovie(movieToDelete);
         //return movieRandomizerService.getMoviesWatched();
